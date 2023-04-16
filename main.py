cf4b4fd2596ed2b25d507e95de453235932682c7
komca12 <57370991+komca12@users.noreply.github.com>
2023-04-16 13:21:14 +0200

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# autor jakub komárek
 
 from scapy.all import *
 from os.path import exists
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     args=sys.argv
     mode=-1
     path=None
+    packetLim=-1
     try: 
         i=1
         
@@ -19,11 +21,14 @@ if __name__ == "__main__":
             
             if arg== "-h" or arg=="--help":
                 print(
-"""Použití: bt-monitor -pcap <file.pcap> -init | -peers | -download
-<file.pcap>: input PCAP file or <file.csv> input CSV file
--init: returns a list of detected bootstrap nodes (IP, port)
--peers: returns a list of detected neighbors (IP, port, node ID, # of conn)
--download: returns file info_hash, size, chunks, contributes (IP+port) """
+"""Nástroj pro analýzu bittorent provozu z pcap soubori
+Použití: bt-monitor -pcap <file.pcap> -init | -peers | -download
+<file.pcap>: vstupní PCAP soubor
+-init: vrací seznam bootstrap uzlů (IP, port)
+-peers: vrací seznam aktivních uzlů - probíhal mezi nimi užitečný provoz (IP, port, node ID, # of conn)
+-download: vrací podrobnosti o stahovaném souboru info_hash, size, chunks, contributes (IP+port)
+-packetLim: horní limit zpracovaných paketů - volitelný argument
+"""
                 )
                 exit(0)
             else:
@@ -41,7 +46,6 @@ if __name__ == "__main__":
                     if mode!=-1:
                         raise Exception("Vyberete právě jeden mód běhu, zkuste -h")
                     mode=0
-                    
                 elif arg=="peers":
                     if mode!=-1:
                         raise Exception("Vyberete právě jeden mód běhu, zkuste -h")
@@ -50,26 +54,29 @@ if __name__ == "__main__":
                     if mode!=-1:
                         raise Exception("Vyberete právě jeden mód běhu, zkuste -h")
                     mode=2
-                        
+                elif arg=="packetLim":
+                    i+=1
+                    if(i>= len(args)):
+                        raise Exception("Chyba v parametrech, zkuste -h")
+                    packetLim=args[i]
                 else:
                     raise Exception("Chyba v parametrech, zkuste -h")
             i+=1
                        
         if  path==None:
-            raise Exception("Nazadal jste soubor")
+            raise Exception("Nazadal jste soubor,zkuste -h")
         elif  not(exists(Path(path)) ):
             raise Exception("Soubor neexistuje")
+        elif  mode==-1:
+            raise Exception("Chyba v parametrech, zkuste -h")
 
     except Exception as ex:
-        print("Error:",ex)
+        print("Chyba:",ex)
         exit(-1)
     
     
-    pcap = rdpcap(path,45000)
+    pcap = rdpcap(path,int(packetLim))
     packetParser=PacketParse(mode)
-
-
-    # Loop through each packet and extract DHT data
 
     for packet in pcap:
         packetParser.parsePacket(packet)
--- packetsParse.py
+++ packetsParse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# autor jakub komárek
 
 import scapy.all as sc
 from os.path import exists
@@ -8,31 +9,94 @@ import re
 
 
 class neighbor:
+    """
+    struktura pro ukládání sousedů
+    """
     ip=None
     port=None
     nodeId=None
     order=0
+    fileHash=None
+    connNum=0
+    sendedPices=0
+    sendedData=0
+    recvData=0
+    recvPices=0
     
-    
-    def __init__(self, ip,port,nodeId,order):
+    def __init__(self, ip,port,nodeId,order,fileHash):
         self.ip=ip
         self.port=port
         self.nodeId=nodeId
         self.order=order
+        self.fileHash=fileHash
+        
     def __repr__(self):
         return "neighbor()"
+    
     def __str__(self):
-        return "%d# Adress:%s:%s , NodeId:%s"%(self.order,self.ip,self.port,self.nodeId.hex())
+        return "%4d# %16s:%5s, InfoHash: %s Dwn/Up:%8d/%8d B, Ratio: %0.3f, Pieces Send/recv %4d/%4d, Connections: %2s"%(self.order,self.ip,self.port,self.nodeId.hex(),
+            self.recvData,self.sendedData, self.sendedData/self.recvData if self.recvData!=0 else 1 , self.recvPices,self.sendedPices ,self.connNum )
+    
+    def incConnNum(self):
+        self.connNum+=1
+    
+    def incData(self,sendedData):
+        self.sendedData+=sendedData
+        self.sendedPices+=1
         
-class file:
-    recvPieces=0
-    sumLen=0
-    neigbors=None
+    def incsendData(self,size):
+        self.recvPices+=1
+        self.recvData+=size
+        
+class fileStat:
+    """
+    struktura obsahující záznamy o stahovaném souboru
+    """
+    contributors=[]
     sha1=None
-    
-    
+    pieceSize=-1
+    recvPieces=0
+    recvData=0
 
+    def __repr__(self):
+        return "fileStat()"
+    
+    def __str__(self):
+        return "Info_hash %s \n\tTotaly sended pieces %d ,Totaly sended %d B, Maximal Piece Size %d B"%(self.sha1.hex(),self.recvPieces,self.recvData,self.pieceSize)
+
+    def __init__(self,sha1,srcIp ):
+        self.sha1=sha1
+        self.srcIp=srcIp
+    
+    def addContributor(self,ip,port):
+        if ip==self.srcIp:
+            return
+        ID=ip+":"+str(port)
+        if not(ID in self.contributors ):
+            self.contributors.append(ID)
+            
+    def setPieceSize(self,new):
+        if self.pieceSize<new:
+            self.pieceSize=new
+            
+    def incSize(self,size):
+        self.recvPieces+=1
+        self.recvData+=size
+            
+    def getSha(self):
+        return self.sha1
+    
+    def getContributors(self):
+        return self.contributors
+    
+    def getInitCont(self):
+        return self.srcIp
+            
 class PacketParse:
+    """
+    hlavní třída nástroje 
+    - ukládá v sobě potřebná data pro analýzu
+    """
     bootStrapDns ={}
     bootStrapPorts ={}
     bootStrapGetPeers =[]
@@ -40,6 +104,9 @@ class PacketParse:
     
     peers={}
     peersCnt=0
+    
+    idsToSha={} #pomocná hasmapa mapující ip na info hash souboru
+    files={}
     
     def __init__(self, mode):
         self.mode=mode
@@ -53,98 +120,117 @@ class PacketParse:
             return True
         return False
 
-
-
     def parsePacket(self,packet):
+        """Funkce podle typu paketu vybere způsob parsování
+        Args:
+            packet: vstupní paket
+        """
         try:
-            if packet.haslayer(sc.UDP):
-                
+            if packet.haslayer(sc.UDP):    
                 if packet.haslayer(sc.DNS):
                     self.dnsParsing(packet)
                 else :
                     payload=packet[sc.Raw].load
                                         
-                    if len(payload)>20 and payload[0:19]==b'.BitTorrentprotocol':
-                        print("handshake")
-                    elif payload[0]==0x64: #d
+                    if payload[0]==0x64: #d
                         if payload[1]==0x31 or payload[1]==0x32:
                             self.dhtParsing(packet,payload)           
             elif  packet.haslayer(sc.TCP):
-                #print(packet)
                 payload=packet[sc.Raw].load
-                self.bittorentParsing(packet,payload)
-                    
-                    
+                self.bittorentParsing(packet,payload)      
+                                           
         except Exception as ex:
             ...
-            print("exeption: ",ex)
-            
+            #print("exeption: ",ex) 
 
-    def dnsParsing(self,packet):
-            
+    def dnsParsing(self,packet):        
         if packet[sc.DNS].qr == 1:
-            # extract the domain name from the response
             domain_name = packet[sc.DNSQR].qname.decode('utf-8')
-            # extract the IP address from the response
             ip_address = packet[sc.DNSRR].rdata
-            # print the domain name and IP address
-            #print("Domain Name: ", domain_name)
-            #print("IP Address: ", ip_address)
             if self.is_valid_ip(ip_address):
                 self.bootStrapDns[ip_address]=domain_name
 
         
-    def bittorentParsing(self,packet,payload):
-        if payload[4]==0x04:
-            print("have piece")
-            print(packet)
-        elif payload[4]==0x07:
-            print("piece")
-            print(packet)
-        elif payload[4]==0x14:
-            print("extended")
-            print(packet)
-        elif payload[4]==0x02:
-            print("interested")
-            print(packet)
-        elif payload[4]==0x06:
-            print("reguest")
-            print(packet)
-        elif payload[4]==0x01:
-            print("unchoke")
-            print(packet)
-        elif payload[4]==0x00:
-            print("choke")
-            print(packet)
-        elif payload[0]==0x13 and payload[1:20]==b'BitTorrent protocol':
-            print("handshake")#28
-            sha1=payload[28:48]
-            peerId=payload[48:68]
+    def bittorentParsing(self,packet,packetPayload):
+        if packetPayload[0]==0x13 and packetPayload[1:20]==b'BitTorrent protocol': #handshake
+            sha1=packetPayload[28:48]
+            peerId=packetPayload[48:68]
             
             ip=packet[sc.IP].dst
             port=packet[sc.TCP].dport
             
-           
-            self.peers[peerId]=neighbor(ip,port,peerId,self.peersCnt)
-            self.peersCnt+=1
+            ipSrc=packet[sc.IP].src
+ 
+            if (self.mode==1  or self.mode==2):
+                if not(ip in self.peers.keys()):
+                    self.peers[ip]=neighbor(ip,port,peerId,self.peersCnt,sha1)
+                    self.peersCnt+=1
+                if  ipSrc in self.peers.keys():                     
+                    self.peers[ip].incConnNum()
+                if not(sha1 in self.files.keys() ):
+                    self.files[sha1]=fileStat(sha1,ipSrc)
+                self.idsToSha[ip]=sha1
+                self.files[sha1].addContributor(ip,port)
+        else:            
+            shift=0
+            while True:  
+                if shift+4>=len(packetPayload):   
+                    break     
+                lenght=int.from_bytes(packetPayload[shift:shift+4], "big") +4
+                
+                if lenght+shift>len(packetPayload):
+                    payload=packetPayload[shift:]
+                else:
+                    payload=packetPayload[shift:shift+lenght]
+                
+                
+                if payload[4]==0x07:
+                    ip=packet[sc.IP].src
+                    ipDst=packet[sc.IP].dst
+                    port=packet[sc.TCP].sport
+                    
+                    size=int.from_bytes(payload[0:4], "big")-9
+                    
+                    if(size>262144):
+                        return                   
+                    if not(ip in self.peers.keys()):
+                        self.peers[ip]=neighbor(ip,port,peerId,self.peersCnt,b"")
+                    self.peers[ip].incData(size)   
+                    self.peers[ipDst].incsendData(size)
+                    if not(ip in self.idsToSha.keys()):
+                        return
+                    sha=self.idsToSha[ip]                    
+                    self.files[sha].setPieceSize(size)
+                    self.files[sha].incSize(size)  
+                    self.files[sha].addContributor(ip,port) 
+                """
+                elif payload[4]==0x05:
+                    print("bitField")
+                elif payload[0]==0x14:
+                    print("extended")
+                    print(packet)
+                elif payload[0]==0x02:
+                    print("interested")
+                    print(packet)
+                elif payload[0]==0x06:
+                    print("reguest")
+                    print(packet)
+                elif payload[0]==0x01:
+                    print("unchoke")
+                    print(packet)
+                elif payload[0]==0x00:
+                    print("choke")
+                    print(packet)
+                elif payload[0]==0x04:
+                    print("have piece")
+                    print(packet)
+                """
+                shift+=lenght
+                if shift>=len(packetPayload):
+                    break
             
-
-
-
-    def dhtParsingPayload(self,payload):
-        keyVals={}
-        lastIndex=0
-        for i in range(0, payload):
-            if payload[i]==ord(':'):
-                keyVals.append()
-
-    def dhtParsing(self,packet,payload):
-        
-        if payload[1]==0x31:
-            clientPacket=True
-        elif payload[1]==0x32:
-            clientPacket=False
-            
+                
+    def dhtParsing(self,packet,payload):            
         strList=payload.split(b':')
         
         if(self.mode==0):
@@ -156,7 +242,6 @@ class PacketParse:
                 self.bootStrapPorts[packet[sc.IP].dst]=packet[sc.UDP].dport
     
     def printResults(self):
-
         if self.mode==0:
             print("Bootstraps servers:")
             for key, value in self.bootStrapDns.items():
@@ -164,6 +249,22 @@ class PacketParse:
                     port=self.bootStrapPorts[key]
                     print(key,":",port," (",value,")",sep="")
         elif self.mode==1:
-            print("Active neighbors:")
+            print("Active peers:")
             for key, value in self.peers.items():
                 print(value)
+        elif self.mode==2:
+            print("Files:")
+            for key, value in self.files.items():
+                print(value)
+                init=value.getInitCont()
+                print("\tDownload begined by:\n\t",self.peers[init])
+                
+                contributors=value.getContributors()
+                print("\tPeers:")
+                for cont in contributors:
+                    id=cont.split(":")[0]
+                    if(id in self.peers):
+                        print("\t",self.peers[id])
+                    else:
+                        print("\t",cont)
++
